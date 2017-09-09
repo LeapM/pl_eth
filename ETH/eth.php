e5fa47a4927ccd498a2d8b05b95797e580e38dd1
leapm <graphql.js@gmail.com>
2017-09-09 21:25:24 +0800

    beta released

--- ETH/eth.php
+++ ETH/eth.php
@@ -1,49 +1,61 @@
 <?php
-//TODO. not sure if I should use require or include. ask Nonu later
-require_once('ethereum-php/ethereum.php');
-$ethNode = '192.168.1.131';
-$ethereum = new Ethereum('localhost', 8545);
-echo "starting etherum\n";
-  function findUserTransaction($address){
-    $txs = getTransactionsByAddress($address,getReliableBlockNo(),
-    getLastCachedBlockNoByAddress($address));
-    return $txs;
-  }
-  function getLastCachedBlockNoByAddress($address)
+require_once ('ethereum-php/ethereum.php');
+
+/*port 8545 is not visible. create SSH tunnel and use localhost
+ssh -fN -L 8545:127.0.0.1:8545 root@192.168.1.131
+*/
+
+// $ethNode = '192.168.1.131';
+
+$ethNode = 'localhost';
+$ethereum = new Ethereum($ethNode, 8545);
+
+function getLastCachedBlockNoByAddress($address)
   {
-    //TODO. Need to add database suppport. For now return the blocknum when pre sale started
-    global $ethereum;
-    //return $ethereum->eth_blockNumber() -6;
-    return '0x40d3c0' - 0;
-   //return 11111;
+
+  // TODO.It is very costly to scan all eth blocks.
+  // May need to store the transaction information in a database(mongodb)
+  // For now, just returen the last 1000 block
+
+  global $ethereum;
+  return $ethereum->eth_blockNumber() - 1000;
   }
 
-  function getReliableBlockNo(){
-    /* return the  in ether blockchain which was confirmed in n blocks
-    TODO check with Nanu if the confirmationNo is required
-    */
-    global $ethereum;
-    $confirmNo = 5;
-    //return $ethereum->eth_blockNumber() - 5;
-    return '0x40d3c0' - 0;
+function getReliableBlockNo()
+  {
+
+  // instead of the latest block,return
+
+  global $ethereum;
+  $confirmNo = 5;
+  return $ethereum->eth_blockNumber() - 5;
   }
 
-  function getTransactionsByAddress($address, $startBlockNumber, $endBlockNumber) {
-    global $ethereum;
-    for ($i = $startBlockNumber; $i <= $endBlockNumber; $i++) {
-      $hexAddress =  '0x' . dechex($i);
-      $block = $ethereum->eth_getBlockByNumber($hexAddress, true);
-      if ($block !== null && $block->transactions !== null) {
-       forEach($block->transactions as $tx){
-        if($tx->to == $address){
-          $result[]=$tx;
+function getTransactionsByAddress($address, $startBlockNumber, $endBlockNumber)
+  {
+  global $ethereum;
+  for ($i = $startBlockNumber; $i <= $endBlockNumber; $i++)
+    {
+    $hexAddress = '0x' . dechex($i);
+    $block = $ethereum->eth_getBlockByNumber($hexAddress, true);
+    if ($block !== null && $block->transactions !== null)
+      {
+      forEach($block->transactions as $tx)
+        {
+        if ($tx->to == $address)
+          {
+          $result[] = $tx;
+          }
         }
-       }
+      }
     }
-  }
-  //TODO. add this to cache
+
+  // TODO. add this to cache
+
   return $result;
-}
+  }
+
 $testAddress = '0x90bd44f0bf4ee16a0cf94fc2154616bebe9f7b06';
-echo json_encode(getTransactionsByAddress($testAddress,getLastCachedBlockNoByAddress($testAddress),getReliableBlockNo()));
+$txs = getTransactionsByAddress($testAddress, getLastCachedBlockNoByAddress($testAddress) , getReliableBlockNo());
+echo json_encode($txs);
 ?>
